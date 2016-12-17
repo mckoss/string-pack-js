62d605e6968a454115f7e06c266566bbd19e31c4
Mike Koss <koss@google.com>
2016-12-16 17:15:06 -0800

    one-string test

--- src/test/pack-test.ts
+++ src/test/pack-test.ts
@@ -3,10 +3,16 @@ import { assert } from 'chai';
 import { pack } from '../pack';
 
 const NO_STRINGS = "x=1;";
+const ONE_STRING = "x=\'hello\';";
 
 suite("pack", () => {
   test("no-strings", () => {
     const result = pack(NO_STRINGS);
     assert.equal(result, NO_STRINGS);
+  });
+
+  test("one-string", () => {
+    const result = pack(ONE_STRING);
+    assert.equal(result, ONE_STRING);
   });
 });
