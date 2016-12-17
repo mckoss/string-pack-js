9c0e3caf8da3ff7d1e979f546fae2b3b655374bd
Mike Koss <koss@google.com>
2016-12-17 11:51:11 -0800

    Remove redundant tests.

--- src/test/statistic-test.ts
+++ src/test/statistic-test.ts
@@ -4,12 +4,6 @@ import { dataDrivenTest } from './data-test-helper';
 import { Statistic } from '../statistic';
 
 suite("Statistic", () => {
-  test("empty", () => {
-    let s = new Statistic();
-    assert.equal(s.count, 0);
-    assert.isUndefined(s.average);
-  });
-
   let tests = [
     { data: [], expect: { count: 0, average: undefined } },
     { data: [100], expect: { count: 1, average: 100 } },
@@ -21,12 +15,5 @@ suite("Statistic", () => {
     data.forEach((x) => stats.sample(x));
     assert.equal(stats.count, expect.count);
     assert.equal(stats.average, expect.average);
-  });
-
-  test("one", () => {
-    let s = new Statistic();
-    s.sample(100);
-    assert.equal(s.count, 1);
-    assert.equal(s.average, 100);
   });
 });
