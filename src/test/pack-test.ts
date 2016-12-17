a705b73d8b3be5502bafe91d6e613bbd7ff0db07
Mike Koss <koss@google.com>
2016-12-17 08:45:40 -0800

    Test with firebase source.

--- src/test/pack-test.ts
+++ src/test/pack-test.ts
@@ -1,18 +1,43 @@
 import { assert } from 'chai';
 
-import { pack } from '../pack';
+import * as fs from 'fs';
 
-const NO_STRINGS = "x=1;";
-const ONE_STRING = "x=\'hello\';";
+import { pack } from '../pack';
+import { zipSize } from '../util';
+
+const TEST_DATA_DIR = process.env.PROJ_DIR + '/src/test/data';
 
 suite("pack", () => {
   test("no-strings", () => {
-    const result = pack(NO_STRINGS);
-    assert.equal(result, NO_STRINGS);
+    const result = pack('x=1;');
+    assert.equal(result, 'x=1;');
   });
 
   test("one-string", () => {
-    const result = pack(ONE_STRING);
+    const result = pack("x=\'hello\';");
     assert.equal(result, "x=_.a;");
+  });
+
+  test("two-strings", () => {
+    const result = pack("x=\'hello\';y='goodbye';");
+    assert.equal(result, "x=_.a;y=_.b;");
+  });
+
+  test("firebase.js", () => {
+    const binary = fs.readFileSync(TEST_DATA_DIR + '/firebase.js', 'utf8');
+    const packed = pack(binary);
+    const data = [binary, packed];
+    const info = data.map((c) => {
+      let result: string[] = [];
+      result.push("Unpacked size: " + c.length);
+      result.push("Packed size: " + zipSize(c));
+      result.push("Sample: ");
+      const codeStart = c.indexOf('var firebase');
+      result.push(c.slice(codeStart, codeStart + 300));
+      return result.join('\n');
+    });
+    console.log(info.join('\n========\n'));
+    assert.isBelow(packed.length, binary.length);
+    assert.isBelow(zipSize(packed), zipSize(binary));
   });
 });
