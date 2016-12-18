cce9407deb3625dc8faac96caa896d14aa6f4da9
Mike Koss <koss@google.com>
2016-12-18 10:25:16 -0800

    Analyze - but typescript error.

--- src/pack.ts
+++ src/pack.ts
@@ -18,6 +18,7 @@ const GENERATE_OPTIONS = {
 };
 
 const STRING_TABLE = '_';
+const MIN_STRING_SIZE = 2;
 
 export function stringStats(program: string): {[key: string]: Statistic} {
   const ast = esprima.parse(program);
@@ -41,11 +42,51 @@ export function stringStats(program: string): {[key: string]: Statistic} {
   return { lengths, duplicates };
 }
 
+class StringTable {
+  counts: {[s: string]: number} = {};
+  mapping: {[s: string]: number} = {};
+
+  add(s: string) {
+    if (s.length < MIN_STRING_SIZE) {
+      return;
+    }
+    if (this.counts[s] === undefined) {
+      this.counts[s] = 0;
+    }
+    this.counts[s] += 1;
+  }
+
+  analyze() {
+    let duplicates: Array<[string, number]> = Object.keys(this.counts)
+      .filter((s) => this.counts[s] > 1)
+      .map((s) => [s, this.counts[s]]);
+    // Descending by count.
+    duplicates.sort((a, b) => b[1] - a[1]);
+
+    let i = 0;
+    for (let pair of duplicates) {
+      this.mapping[pair[0]] = i;
+      i += 1;
+    }
+  }
+}
+
 // Transpile a JavaScript program by replacing its strings with shared
 // references to a shared string table.
 export function pack(program: string): string {
   let iProp = 0;
   let ast = esprima.parse(program, PARSE_OPTIONS);
+  let strings = new StringTable();
+
+  estraverse.traverse(ast, {
+    enter: (node, _) => {
+      if (node.type === 'Literal' && typeof(node.value) === 'string') {
+        strings.add(node.value);
+      }
+    }
+  });
+
+  strings.analyze();
 
   estraverse.replace(ast, {
     enter: (node, _) => {
