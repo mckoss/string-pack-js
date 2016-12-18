85469cb97698194de9d78ede67c7e0a2e6758811
Mike Koss <koss@google.com>
2016-12-17 17:15:02 -0800

    Histograms.

--- src/statistic.ts
+++ src/statistic.ts
@@ -2,9 +2,17 @@ export class Statistic {
   count = 0;
   sum = 0;
   sumSquares = 0;
+  histogram: number[] = [];
 
   min: number;
   max: number;
+
+  // Define an infite collection of histogram buckets based on this sequence of
+  // break points. Each number defined the low end of the range of a bucket. We
+  // extrapolate an infinite number of buckets based on the difference of the
+  // last two listed.
+  constructor(private buckets?: number[]) {
+  }
 
   get average(): number | undefined {
     if (this.count === 0) {
@@ -17,5 +25,32 @@ export class Statistic {
     this.sum += x;
     this.sumSquares += x * x;
     this.count += 1;
+
+    if (!this.buckets || this.buckets.length === 0) {
+      return;
+    }
+
+    let i = 0;
+    let delta = 1;
+    let base = this.buckets[0];
+    let next: number;
+    while (true) {
+      if (this.histogram[i] === undefined) {
+        this.histogram[i] = 0;
+      }
+      if (i + 1 < this.buckets.length) {
+        next = this.buckets[i + 1];
+        delta = next - base;
+      } else {
+        next = base + delta;
+      }
+      if (x >= base && x < next) {
+        break;
+      }
+      i += 1;
+      base = next;
+    }
+
+    this.histogram[i] += 1;
   }
 }
--- src/test/statistic-test.ts
+++ src/test/statistic-test.ts
@@ -5,15 +5,27 @@ import { Statistic } from '../statistic';
 
 suite("Statistic", () => {
   let tests = [
-    { data: [], expect: { count: 0, average: undefined } },
-    { data: [100], expect: { count: 1, average: 100 } },
-    { data: [1, 2, 3], expect: { count: 3, average: 2 } },
+    { data: {samples: []},
+      expect: { count: 0, average: undefined } },
+    { data: {samples: [100], buckets: [0, 20]},
+      expect: { count: 1, average: 100, histogram: [0, 0, 0, 0, 0, 1] } },
+    { data: {samples: [1, 2, 3], buckets: [1]},
+      expect: { count: 3, average: 2, histogram: [1, 1, 1] } },
+    { data: {samples: [1, 2, 3, 10, 100, 15, 4], buckets: [1, 10, 100]},
+      expect: { histogram: [4, 2, 1] } },
   ];
 
-  dataDrivenTest(tests, (data: number[], expect: any) => {
-    let stats = new Statistic();
-    data.forEach((x) => stats.sample(x));
-    assert.equal(stats.count, expect.count);
-    assert.equal(stats.average, expect.average);
+  dataDrivenTest(tests, (data: any, expect: any) => {
+    let stats = new Statistic(data.buckets);
+    data.samples.forEach((x: number) => stats.sample(x));
+    if (expect.count != undefined) {
+      assert.equal(stats.count, expect.count);
+    }
+    if (expect.average !== undefined) {
+      assert.equal(stats.average, expect.average);
+    }
+    if (expect.histogram !== undefined) {
+      assert.deepEqual(stats.histogram, expect.histogram);
+    }
   });
 });
